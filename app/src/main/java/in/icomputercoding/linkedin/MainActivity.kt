de08d89985b7f0fe8d34b705c1895b2743953cc4
Vaibhav Shivaji Wabale <57138983+vaibhavwabale@users.noreply.github.com>
2022-10-06 22:45:24 +0530

    Initial commit

---
+++ app/src/main/java/in/icomputercoding/linkedin/MainActivity.kt
@@ -0,0 +1,25 @@
+package `in`.icomputercoding.linkedin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this,SignIn::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+
+    }
+}
+
+
