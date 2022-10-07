46c06af9ee6785480f181a1cbac425f546fd2f2f
Vaibhav Shivaji Wabale <57138983+vaibhavwabale@users.noreply.github.com>
2022-10-07 08:37:20 +0530

    Added SignIn Layout

--- app/src/main/java/in/icomputercoding/linkedin/SignIn.kt
+++ app/src/main/java/in/icomputercoding/linkedin/SignIn.kt
@@ -1,11 +1,21 @@
 package `in`.icomputercoding.linkedin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class SignIn : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val signIn: AppCompatButton = findViewById(R.id.signIn)
+        signIn.setOnClickListener {
+            val intent = Intent(this,SignInLayout::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/in/icomputercoding/linkedin/SignInLayout.kt
@@ -0,0 +1,11 @@
+package `in`.icomputercoding.linkedin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_layout)
+    }
+}
